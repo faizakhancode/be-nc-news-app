7bfdd9ebcce584bfb70fb8cf07edf68d397635e6
faizakhancode <faizakhan488@hotmail.co.uk>
2022-02-16 10:27:23 +0000

    added ; to sql

--- models/news.model.js
+++ models/news.model.js
@@ -2,7 +2,7 @@ const db = require('../db/connection');
 
 //#3 endpoint model:
 exports.fetchTopics = () => {
-  let queryStr = `SELECT * FROM topics`;
+  let queryStr = `SELECT * FROM topics;`;
   return db.query(queryStr).then(({ rows: topics }) => {
     return topics;
   });
